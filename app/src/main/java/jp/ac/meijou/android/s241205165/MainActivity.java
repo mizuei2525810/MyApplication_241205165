ad7d8c39da54361e41a2c923a310e65f90e5a705
Eito Mizuno <185331637+mizuei2525810@users.noreply.github.com>
2025-09-02 15:41:12 +0900

    ・テキストの表示 ・イメージの表示 ・テキストの入力 ・DataStoreの実装

--- app/src/main/java/jp/ac/meijou/android/s241205165/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s241205165/MainActivity.java
@@ -16,6 +16,8 @@ import jp.ac.meijou.android.s241205165.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private PrefDataStore prefDataStore;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +29,11 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        prefDataStore = PrefDataStore.getInstance(this);
+        binding.saveButton.setOnClickListener(view -> {
+            var text = binding.editTextText.getText().toString();
+            prefDataStore.setString("name", text);
         });
 
         binding.text.setText(R.string.text1);
@@ -53,6 +60,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
     }
+    protected void onStart() {
+        super.onStart();
+        prefDataStore.getString("name")
+                .ifPresent(name -> binding.text.setText(name));
+    }
+
 }
